0ca56fab2f829eb2ffabe98679928c64c60aa69c
Antoni Angga <antoniangga14@gmail.com>
2017-08-09 10:52:06 +0700

    Seeder di tambahkan

---
+++ Backend/seeders/20170809032148-mechanic_seeder.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert('mechanics', [{
+        id_ktp: '12312312313123',
+        name: 'Sidik',
+        address: 'jl. Bintaro sektor 8',
+        gender: 'Male',
+        password: '1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
