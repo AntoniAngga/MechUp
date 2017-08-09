b918dd9e6e4b61ac7965bef8fb5f27a20fef0656
Antoni Angga <antoniangga14@gmail.com>
2017-08-09 14:31:15 +0700

    testing + table tadi kelupaan tambah vehicle nya

---
+++ Backend/seeders/20170809065905-vehicle_seeding.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('vehicles', [{
+      type: "Nissan Juke",
+      tahun: "2017",
+      merek: "Nissan",
+      id_customer: 1
+    },{
+      type: "BMW 320I",
+      tahun: "2014",
+      merek: "BMW",
+      id_customer: 1
+    }])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('vehicles', null, {})
+  }
+};
