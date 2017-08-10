6324e6cc7f5e66a7119e6e232bbb612773c1da21
Antoni Angga <antoniangga14@gmail.com>
2017-08-10 11:49:03 +0700

    Mechanic untuk test dan api nya sudah bisa

--- Backend/routes/mechanic.js
+++ Backend/routes/mechanic.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var cCustomer = require('../controller/cMechanic');
+var cMechanic = require('../controller/cMechanic');
 
 /* GET home page. */
-router.get('/', );
+router.get('/', cMechanic.findall_get);
+router.get('/:id', cMechanic.findbyid_get);
+router.post('/', cMechanic.create_post);
+router.put('/:id', cMechanic.edit_put);
+router.delete('/:id', cMechanic.destory_delete);
 
 module.exports = router;
